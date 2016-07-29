24c7e36eee146f635454129b66ba706357a349bc
Joseph Li <josephyli@gmail.com>
2016-07-29 13:41:09 -1000

    Enter key submits login, and textfield is auto-selected

--- loginController.java
+++ loginController.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -26,7 +27,7 @@ import javax.naming.NamingException;
  * @author watan
  */
 public class loginController implements Initializable {
-    
+
     @FXML
     private Label lblUser;
     @FXML
@@ -39,30 +40,43 @@ public class loginController implements Initializable {
     private TextField txtPass;
     @FXML
     private Button submit;
-    
-    
+
     public void Login(ActionEvent event) throws IOException, SQLException, NamingException {
         UserBean userBean = new UserBean();
         String name = txtUser.getText();
         String password = txtPass.getText();
-        
-        if(userBean.checkUser(name, password)){
+
+        if (userBean.checkUser(name, password)) {
             System.out.println("Login Successful");
             User user = new User();
             user.setName(name);
             Parent root = FXMLLoader.load(getClass().getResource("sleep.fxml"));
+
             Scene scene = new Scene(root);
             Stage stage = new Stage();
             stage.setScene(scene);
             stage.show();
-        }
-        else{
+        } else {
             status.setText("Login Failed.  Invalid Username/Password");
         }
     }
-    
+
+    // pressing enter key in a textfield, runs the Login method
+    public void onEnter() throws NamingException, IOException, SQLException {
+        ActionEvent event = null;
+
+        Login(event);
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                txtUser.requestFocus();
+            }
+        });
+
+    }
 }
